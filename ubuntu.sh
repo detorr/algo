15670561f78c3ad6b84dbb23a0d2a564f4fc5eb0
McSwanky <detorr@live.com>
2019-11-15 01:58:25 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,8 +2,8 @@
 git clone https://github.com/trailofbits/algo.git &&
   cd algo &&
 #install dependencies
-  sudo apt update && apt -y upgrade \\
-  sudo apt install -y python3-virtualenv python3-pip && \\
+  sudo apt update && apt -y upgrade &&
+  sudo apt install -y python3-virtualenv python3-pip && 
     python3 -m virtualenv --python="$(command -v python3)" .env &&
       source .env/bin/activate &&
       python3 -m pip install -U pip virtualenv &&
